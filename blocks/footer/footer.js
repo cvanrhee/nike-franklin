0523a69dfc38da2541466164ef72b804f073e07a
cvanrhee <cvanrhee@adobe.com>
2023-03-03 12:55:15 -0700

    Updating some CSS on the footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -8,12 +8,25 @@ import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
-
   const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
   const html = await resp.text();
   const footer = document.createElement('div');
   footer.innerHTML = html;
+
+  var links = footer.getElementsByClassName('links')[0];
+  var container = links.getElementsByTagName('div')[0];
+  container.className = 'grid';
+
+  var rows = links.getElementsByTagName('ul');
+  Array.from(rows).forEach(element => {
+    element.className = 'link';
+  });
+
+  var copyright = footer.getElementsByClassName('copyright')[0];
+  var container = copyright.getElementsByTagName('div')[0];
+  container.className = 'grid';
+
   await decorateIcons(footer);
   block.append(footer);
 }
